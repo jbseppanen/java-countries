a64dd9ebde1b9999a76feba111f3c2dc6b1cd955
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-11 15:10:31 -0800

    Implement the rest of the methods in the CountryController.

--- src/main/java/com/lambdaschool/javacountries/CountryController.java
+++ src/main/java/com/lambdaschool/javacountries/CountryController.java
@@ -1,10 +1,11 @@
 package com.lambdaschool.javacountries;
 
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestParam;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 
 @RestController
@@ -22,9 +23,41 @@ public class CountryController {
 
     @RequestMapping("names/size")
     public ArrayList<Country> getCountriesGreaterThanByNameLength(@RequestParam(value = "letters") int length) {
-        return WebCountriesApplication.list.findCountries(e -> (e.getName().length()>length));
+        return WebCountriesApplication.list.findCountries(e -> (e.getName().length() > length));
     }
 
+    @RequestMapping("population/size")
+    public ArrayList<Country> getCountriesGreaterThanByPop(@RequestParam(value = "people") int pop) {
+        return WebCountriesApplication.list.findCountries(e -> (e.getPopulation() > pop));
+    }
 
+    @RequestMapping("population/min")
+    public Country getCountriesPopMin() {
+        WebCountriesApplication.list.countryList.sort(Comparator.comparingLong(Country::getPopulation));
+        return WebCountriesApplication.list.countryList.get(0);
+    }
+
+    @RequestMapping("population/max")
+    public Country getCountriesPopMax() {
+        WebCountriesApplication.list.countryList.sort(Comparator.comparingLong(Country::getPopulation));
+        return WebCountriesApplication.list.countryList.get(WebCountriesApplication.list.countryList.size()-1);
+    }
+
+    @RequestMapping("age/age")
+    public ArrayList<Country> getCountriesGreaterThanByAge(@RequestParam(value = "age") int ageInput) {
+        return WebCountriesApplication.list.findCountries(e -> (e.getPopulation() > ageInput));
+    }
+
+    @RequestMapping("age/min")
+    public Country getCountriesAgeMin() {
+        WebCountriesApplication.list.countryList.sort(Comparator.comparingLong(Country::getAge));
+        return WebCountriesApplication.list.countryList.get(0);
+    }
+
+    @RequestMapping("age/max")
+    public Country getCountriesAgeMax() {
+        WebCountriesApplication.list.countryList.sort(Comparator.comparingLong(Country::getAge));
+        return WebCountriesApplication.list.countryList.get(WebCountriesApplication.list.countryList.size()-1);
+    }
 
 }
